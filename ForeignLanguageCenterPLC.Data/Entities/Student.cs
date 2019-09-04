2211c3ec0f76a18ab649cf8efe8ef72048ce6816
ThanHoang <ThanHoang@DESKTOP-F99ED5G>
2019-09-04 16:41:38 +0700

    oải vc

---
+++ ForeignLanguageCenterPLC.Data/Entities/Student.cs
@@ -0,0 +1,17 @@
+﻿using ForeignLanguageCenterPLC.Infrastructure.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ForeignLanguageCenterPLC.Data.Entities
+{
+    [Table("Students")]
+    public class Student : DomainEntity<int>
+    {
+        public string StudentCode { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ ForeignLanguageCenterPLC/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForeignLanguageCenterPLC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryType()
+        {
+            return View();
+        }
+    }
+}
